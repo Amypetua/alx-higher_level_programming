7b6821285ef53c7eafe6fe0a11354e098d6f89df
Amypetua <anachusichiamaka32@gmail.com>
2023-03-12 12:38:52 -0700

    Fifty-nine

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:02d},".format(i),  end=" ")
+for i in range(0, 100):
+    print("{}{}".format((i // 10), (i % 10)), end="" if i == 99 else ", ")
