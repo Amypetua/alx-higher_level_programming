ff460469553784717fc85e030d07647252179dfe
Amypetua <anachusichiamaka32@gmail.com>
2023-03-12 14:09:12 -0700

    Invention is a combination of brains

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for a in range(0, 10):
-    for b in range(a + 1, 10):
-        if a == 8 and y == 9:
-            print("89")
-        else:
-        print("{:d}{:d}, ".format(a, b), end=", ")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=", ")
+    print("\n")
