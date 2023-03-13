97326416669ada2e32eb97159e88d6d49963cc61
Amypetua <anachusichiamaka32@gmail.com>
2023-03-12 23:02:00 -0700

    Invention

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=" " if i == 8 and j = 9 else end=", ")
+        if i == 8 and j == 9:
+            print("89")
+        else:
+            print("{}{},".format(i, j), end=" ")
