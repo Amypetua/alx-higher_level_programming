90843b8536981910a3c2805cd0c52de195d8a4c3
Amypetua <anachusichiamaka32@gmail.com>
2023-03-13 13:20:36 -0700

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for char in range(ord('z') and ord('a') - 1, -1):
-    print(chr(char), end='')
-    char -= 1
-    if char >= ord('A'):
-        print(chr(char - ord('a') + ord('A')), end='')
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print('{}'.format(chr(c - i)), end='')
+    i = 32 if i == 0 else 0
