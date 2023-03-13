e9733c13a0069dc4910c3d774c2c2f975da127c1
Amypetua <anachusichiamaka32@gmail.com>
2023-03-13 00:04:29 -0700

    Upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,5 @@ def uppercase(str):
     for a in str:
         if ord(a) >= 97 and ord(a) <= 122:
             a = chr(ord(a) - 32)
-
             print('{}'.format(a), end='')
-
-        print('')
+        print('\n')
