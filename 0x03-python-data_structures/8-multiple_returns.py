40d2b2560abc08fbc173eaf3f023773b2f84fc63
Amypetua <anachusichiamaka32@gmail.com>
2023-03-14 10:58:36 -0700

    More return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if lenlen(sentence) == 0:
-        return (0, None)
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
     else:
-        return (len(sentence), sentence[0])
+        my_tuple = len(sentence), sentence[0]
+    return (my_tuple)
