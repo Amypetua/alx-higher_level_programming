79b4843ea1b2447beae354ef8450d1efdcd4372a
Amypetua <anachusichiamaka32@gmail.com>
2023-03-27 14:53:37 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        while True:
+            if count == x:
+                break
+            print(my_list[count], end="")
+            count += 1
+    except:
+        pass
+    print()
+    return count
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This directory contains alx project on python. It focus on Error. Errors are divided into two: (a) syntax or parsing errors  (b) exceptions
