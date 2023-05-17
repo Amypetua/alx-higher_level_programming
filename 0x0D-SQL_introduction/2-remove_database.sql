5c04ef94c87f009b642aa0a5d5dad94989966b51
Amypetua <anachusichiamaka32@gmail.com>
2023-05-16 19:56:08 -0700

    Delete a database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/env sql
+
+-- A script that deletes the database hbtn_0c_0 in my MySQL server.
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements.
+
+DROP DATABASE hbtn_0c_0;
