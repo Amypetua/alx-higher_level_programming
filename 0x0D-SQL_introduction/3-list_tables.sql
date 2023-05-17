d2835deb6f21e8d7e1e4a1080dda2678b7e9a426
Amypetua <anachusichiamaka32@gmail.com>
2023-05-16 20:47:41 -0700

    List tablee

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/env sql
+
+-- A script that lists all the tables of a database in my MySQL server.
+-- The database name will be passed as argument of mysql command.
+
+
+SHOW TABLES FROM mysql;
