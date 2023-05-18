ddc0b011c500d0334119f1fe40ce06049621c85e
Amypetua <anachusichiamaka32@gmail.com>
2023-05-18 11:42:02 -0700

    First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,4 @@
 #!/usr/bin/env sql
-INSERT INTO `first_table` (`id`, `name`) VALUE (89, "Best School");
+-- A script that inserts a new row in the table first_table (database hbtn_0c_0) in my MySQL server.
+
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
