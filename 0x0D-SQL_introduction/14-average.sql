2170cd65c1056d74e1486ef10a0acea5ec54c06c
Amypetua <anachusichiamaka32@gmail.com>
2023-05-18 15:14:10 -0700

    Average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+#!/usr/bin/env sql
+
+-- A script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in my MySQL server
+
+SELECT AVG(score) AS average FROM `second_table`;
