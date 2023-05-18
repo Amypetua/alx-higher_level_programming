a8bf5b10ffdb8ea37e9ceb9c16d78f4dd8ac85ff
Amypetua <anachusichiamaka32@gmail.com>
2023-05-18 01:13:51 -0700

    List Tables

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,9 +1,5 @@
 #!/usr/bin/env sql
 
 -- A script that lists all the tables of a database in my MySQL server.
--- The database name will be passed as argument of mysql command.
-
-USE mysql;
-
 
 SHOW TABLES;
