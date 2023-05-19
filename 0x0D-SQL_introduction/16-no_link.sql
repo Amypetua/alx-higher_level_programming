9309fdafd7f43b382a9623c477e9b4d18eb606cd
Amypetua <anachusichiamaka32@gmail.com>
2023-05-18 19:16:29 -0700

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,4 +2,5 @@
 
 -- A script that lists all records of the table second_table of the database hbtn_0c_0 in my MySQL server
 
-SELECT `score`, `name` FROM `second_tsble` WHERE `name` IS NOT NULL ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `name` IS NOT NULL 
+                   ORDER BY `score` DESC;
