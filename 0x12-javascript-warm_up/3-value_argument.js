d9c7faa473189bcf423630074e83489a59b765f8
Amypetua <anachusichiamaka32@gmail.com>
2023-06-15 00:06:20 -0700

    Values

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const first_argument = process.argv[2];
 if (!first_argument) {
-  console.log('No argument');
-} else {
-  console.log(first_argument);
+   console.log('No argument');
+}  else {
+   console.log(first_argument);
 }
