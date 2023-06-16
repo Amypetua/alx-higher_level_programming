3ab2812e614a79675c07f46d4e20c874bf9d0007
Amypetua <anachusichiamaka32@gmail.com>
2023-06-15 23:40:53 -0700

    An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let firstArgument = process.argv[2];
 if (!firstArgument || isNaN(firstArgument)) {
-  console.log('Not a number);
+  console.log('Not a number');
 } else {
   firstArgument = parseInt(firstArgument);
   console.log('My number: ' + firstAgrument);
