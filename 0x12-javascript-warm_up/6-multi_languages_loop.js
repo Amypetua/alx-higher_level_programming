944554ef7a7403ed6ade59235e8ad2ff7bf06080
Amypetua <anachusichiamaka32@gmail.com>
2023-06-16 00:06:17 -0700

    Multi-language

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < stringArray.length; i++) {
-   console.log(stringArray[i]);
+  console.log(stringArray[i]);
 }
