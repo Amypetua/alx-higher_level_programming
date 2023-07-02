7405fad8db8fc697c95be2d86708e57190c14302
Amypetua <anachusichiamaka32@gmail.com>
2023-07-02 14:49:25 -0700

    Email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,10 @@
 #!/usr/bin//python3
 """ Python modules """
+
 import sys
-import urllib.request
 import urllib.parse
+import urllib.request
+
 """ A Python script that takes in a URL, sends a request to the URL """
 
 
