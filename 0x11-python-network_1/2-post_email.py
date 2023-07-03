6fc3fc809fd6437d1a54edcd2e69b6ee0fb9c71b
Amypetua <anachusichiamaka32@gmail.com>
2023-07-02 20:40:35 -0700

    Post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,14 @@
 #!/usr/bin//python3
-""" Python modules """
+""" 
+A Python script that takes in a URL & and email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
 
 import sys
 import urllib.parse
 import urllib.request
 
-""" A Python script that takes in a URL, sends a request to the URL """
 
 
 if __name__ == "__main__":
