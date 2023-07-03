5c9197c6e5cc552d0f8bc75461d9a060c07858c6
Amypetua <anachusichiamaka32@gmail.com>
2023-07-02 21:03:53 -0700

    What's your status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A a Python script that fetches https://alx-intranet.hbtn.io/statu
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
