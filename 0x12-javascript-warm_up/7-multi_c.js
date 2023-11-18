374c3bb0e5a9aa9981e55d3d42f1163fa9ff02b5
Amypetua <anachusichiamaka32@gmail.com>
2023-11-18 15:25:08 +0000

    C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let firstArgument = process.argv[2];
+if (!firstArgument || isNaN(firstArgument)) {
+  console.log('Missing number of occurrences');
+} else {
+  firstArgument = parseInt(firstArgument);
+  for (let i = 0; i < firstArgument; i++) {
+    console.log('C is fun');
+  }
+}
