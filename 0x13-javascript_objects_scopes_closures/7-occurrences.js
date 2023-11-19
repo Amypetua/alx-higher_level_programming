ce0388804dfcc36fb42ca2c55c6835dc38972af2
Amypetua <anachusichiamaka32@gmail.com>
2023-11-19 13:33:20 +0000

    Java scopes

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
