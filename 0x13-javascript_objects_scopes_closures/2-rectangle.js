26597e9a97089cab9f7daba92be2f9e9001d183a
Amypetua <anachusichiamaka32@gmail.com>
2023-11-19 11:36:57 +0000

    Rectangles

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -6,10 +6,4 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
 };
